95c5cd0fe268d5abdf013624b00bf6beb0e5d1d4
Saurabh-e <saurabhpandey4718@gmail.com>
2026-06-17 14:26:40 +0530

    initial build

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <><div center>Ai blogs</div><ul>
+      <li>AI in Healthcare</li>
+      <li>AI in Finance</li>
+      <li>AI in Education</li>
+      <li>AI in Transportation</li>
+      <li>AI in Entertainment</li>
+      <li>AI in Agriculture</li>
+      <li>AI in Cybersecurity</li>
+      <li>AI in Robotics</li>
+      <li>AI in Natural Language Processing</li>
+      <li>AI in Computer Vision</li>
+    </ul></>
+  )
+}
+
+export default App
