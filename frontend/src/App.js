71d0c2f7ae8dbb7dd5a1eb061e400135123fb0ae
Saurabh-e <saurabhpandey4718@gmail.com>
2026-06-17 15:24:30 +0530

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,18 @@ const App = () => {
       <li>AI in Robotics</li>
       <li>AI in Natural Language Processing</li>
       <li>AI in Computer Vision</li>
-    </ul></>
+    </ul>
+    {/* create form which will contain name and email and message and submit button */}
+    <form>
+      <label htmlFor="name">Name:</label>
+      <input type="text" id="name" name="name" />
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" />
+      <label htmlFor="message">Message:</label>
+      <textarea id="message" name="message"></textarea>
+      <button type="submit">Submit</button>
+    </form>
+    </>
   )
 }
 
